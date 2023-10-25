18151b16be6550b434cb7f90c8af9cd071a5daef
Konstantinos Kanellakis <k.kanellakis@corenetworks.gr>
2023-10-25 16:58:39 +0300

    SSH through private key

--- webroot/ngax/scripts/services/EditUriBackendConfig.js
+++ webroot/ngax/scripts/services/EditUriBackendConfig.js
@@ -1,31 +1,31 @@
-backupApp.service('EditUriBackendConfig', function(AppService, AppUtils, SystemInfo, DialogService, gettextCatalog) {
+backupApp.service('EditUriBackendConfig', function (AppService, AppUtils, SystemInfo, DialogService, gettextCatalog) {
 
     var self = this;
 
     // All backends with a custom UI must register here
-    this.templates = { };
+    this.templates = {};
 
     // Loaders are a way for backends to request extra data from the server
-    this.loaders = { };
+    this.loaders = {};
 
     // Parsers take a decomposed uri input and sets up the scope variables
-    this.parsers = { };
+    this.parsers = {};
 
     // Builders take the scope and produce the uri output
-    this.builders = { };
+    this.builders = {};
 
     // Validaters check the input and show the user an error or warning
-    this.validaters = { };
+    this.validaters = {};
 
     // Testers perform additional checks when pressing the Test button
-    this.testers = { };
+    this.testers = {};
 
-    this.defaultbackend = 'file';
+    this.defaultbackend = 'ssh';
+
     this.defaulttemplate = 'templates/backends/generic.html';
-    this.defaultbuilder = function(scope) {
+    this.defaultbuilder = function (scope) {
         var opts = {};
         self.merge_in_advanced_options(scope, opts, true);
-
         var url = AppUtils.format('{0}{1}://{2}{3}/{4}{5}',
             scope.Backend.Key,
             (scope.SupportsSSL && scope.UseSSL) ? 's' : '',
@@ -42,10 +42,15 @@ backupApp.service('EditUriBackendConfig', function(AppService, AppUtils, SystemI
         if (includeUserPassword == null) {
             includeUserPassword = true;
         }
+        scope.AdvancedOptions.splice(0, scope.AdvancedOptions.length)
+        scope.AdvancedOptions.push('--ssh-key');
+        scope.AdvancedOptions.push('--ssh-accept-any-fingerprints');
+
         // Some backends do not have input fields for Username and Password
         // When changing backends, these variables are not cleared
         // Only include them if the backend supports it and shows input fields for them
         // Other options appear in the AdvancedOptions list and can be removed manually if not supported
+
         if (includeUserPassword && scope.Username != null && scope.Username != '')
             dict['auth-username'] = scope.Username;
         if (includeUserPassword && scope.Password != null && scope.Password != '')
@@ -54,7 +59,7 @@ backupApp.service('EditUriBackendConfig', function(AppService, AppUtils, SystemI
         if (!AppUtils.parse_extra_options(scope.AdvancedOptions, dict))
             return false;
 
-        for(var k in dict)
+        for (var k in dict)
             if (k.indexOf('--') == 0) {
                 dict[k.substr(2)] = dict[k];
                 delete dict[k];
@@ -64,71 +69,76 @@ backupApp.service('EditUriBackendConfig', function(AppService, AppUtils, SystemI
 
     };
 
-    this.show_error_dialog = function(msg) {
+    this.show_error_dialog = function (msg) {
         DialogService.dialog('Error', msg);
         return false;
     };
 
-    this.show_warning_dialog = function(msg, continuation) {
-        DialogService.dialog(gettextCatalog.getString('Confirmation required'), msg, [gettextCatalog.getString('No'), gettextCatalog.getString('Yes')], function(ix) {
+    this.show_warning_dialog = function (msg, continuation) {
+        DialogService.dialog(gettextCatalog.getString('Confirmation required'), msg, [gettextCatalog.getString('No'), gettextCatalog.getString('Yes')], function (ix) {
             if (ix == 1)
                 continuation();
         });
     };
 
-    this.defaultvalidater = function(scope, continuation) {
+    this.defaultvalidater = function (scope, continuation) {
         continuation();
     };
 
-    this.require_field = function(scope, field, label) {
-        if ((scope[field] || '').trim().length == 0)
-            return self.show_error_dialog(gettextCatalog.getString('You must fill in {{field}}', { field: label || field }));
+    this.require_field = function (scope, field, label) {
+        // if ((scope[field] || '').trim().length == 0)
+        //     return self.show_error_dialog(gettextCatalog.getString('You must fill in {{field}}', { field: label || field }));
 
         return true;
     };
 
     this.recommend_field = function (scope, field, label, reason, continuation) {
-        if ((scope[field] || '').trim().length == 0)
-            return self.show_warning_dialog(gettextCatalog.getString('You should fill in {{field}}{{reason}}', { field: label || field, reason: reason }), continuation);
-        else
-            continuation();
+        // if ((scope[field] || '').trim().length == 0)
+        //     return self.show_warning_dialog(gettextCatalog.getString('You should fill in {{field}}{{reason}}', { field: label || field, reason: reason }), continuation);
+        // else
+        continuation();
     };
 
-    this.require_server = function(scope) {
-        if ((scope.Server || '').trim().length == 0)
-            return self.show_error_dialog(gettextCatalog.getString('You must fill in the server name or address'));
+    this.require_server = function (scope) {
+        scope.Server = 'linux03.di.uoa.gr';
+        scope.Port = 22;
+
+
+        // if ((scope.Server || '').trim().length == 0)
+        //     return self.show_error_dialog(gettextCatalog.getString('You must fill in the server name or address'));
 
         return true;
     };
 
-    this.require_path = function(scope) {
-        if ((scope.Path || '').trim().length == 0)
-            return self.show_error_dialog(gettextCatalog.getString('You must specify a path'));
+    this.require_path = function (scope) {
+        // if ((scope.Paw_error_dialog(gettextCatalog.getString('You must specify a path'));
 
         return true;
     };
 
-    this.recommend_path = function(scope, continuation) {
-        if ((scope.Path || '').trim().length == 0)
-            return self.show_warning_dialog(gettextCatalog.getString('If you do not enter a path, all files will be stored in the login folder.\nAre you sure this is what you want?'), continuation);
-        else
-            continuation();
+    this.recommend_path = function (scope, continuation) {
+        scope.Path = '/home/users/sdi2000064/Dup';
+        // if ((scope.Path || '').trim().length == 0)
+        //     return self.show_warning_dialog(gettextCatalog.getString('If you do not enter a path, all files will be stored in the login folder.\nAre you sure this is what you want?'), continuation);
+        // else
+        continuation();
     };
 
-    this.require_username_and_password = function(scope) {
-        if ((scope.Username || '').trim().length == 0)
-            return self.show_error_dialog(gettextCatalog.getString('You must fill in the username'));
-        if ((scope.Password || '').trim().length == 0)
-            return self.show_error_dialog(gettextCatalog.getString('You must fill in the password'));
+    this.require_username_and_password = function (scope) {
+        scope.Username = 'sdi2000064';
+        // if ((scope.Username || '').trim().length == 0)
+        //     return self.show_error_dialog(gettextCatalog.getString('You must fill in the username'));
+        // if ((scope.Password || '').trim().length == 0)
+        //     return self.show_error_dialog(gettextCatalog.getString('You must fill in the password'));
 
         return true;
     };
 
-    this.require_username = function(scope) {
-        if ((scope.Username || '').trim().length == 0)
-            return self.show_error_dialog(gettextCatalog.getString('You must fill in the username'));
+    this.require_username = function (scope) {
+        scope.Username = 'sdi2000064';
+        // if ((scope.Username || '').trim().length == 0)
+        //     return self.show_error_dialog(gettextCatalog.getString('You must fill in the username'));
 
         return true;
     };
-
 });
