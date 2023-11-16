1d024c0bf646bc65b9ab1e4bf75454602282e831
Kanellaman <kanellakhskostas@gmail.com>
2023-11-16 12:41:06 +0200

    Add Backup Change Only one backup can be done

--- webroot/ngax/scripts/controllers/AddWizardController.js
+++ webroot/ngax/scripts/controllers/AddWizardController.js
@@ -1,12 +1,20 @@
-backupApp.controller('AddWizardController', function($scope, $location, gettextCatalog) {
-
+backupApp.controller('AddWizardController', function($scope, $location, EditUriBackendConfig, gettextCatalog, BackupList) {
     $scope.selection = {
         style: 'blank'
     };
-
     $scope.nextPage = function() {
+        $scope.backups = BackupList.watch($scope);
+        console.log($scope.backups.length)
         if ($scope.selection.style == 'blank')
-            $location.path('/add');
+        {
+            if($scope.backups.length === 0)
+                $location.path('/add');
+            else if($scope.backups.length === 1)
+            {
+                EditUriBackendConfig.show_error_dialog(gettextCatalog.getString('Maximum number of Backups reached. You can edit or delete your Backup by clicking the down arrow at the home page'))
+                $location.path('/');
+            }
+        }
         else
             $location.path('/import');
     };
